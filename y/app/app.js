d5460a6c193bda7453b9f105fa1547d8fdac191e
Ring Sumlut <ringsumlut@gmail.com>
2021-10-31 20:56:21 -0400

    first commit

---
+++ y/app/app.js
@@ -0,0 +1,33 @@
+const signUpForm = document.querySelector('#signup-form');
+
+signUpForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const firstname = signUpForm['firstname'].value;
+    const lastname = signUpForm['lastname'].value;
+    const email = signUpForm['signup-email'].value;
+    const password = signUpForm['signup-password'].value;
+
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred);
+        alert("Hi, ", firstname.value, lastname.value)
+    })
+
+})
+
+const signInForm = document.querySelector('#signin-form');
+
+signInForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = signInForm['signin-email'].value;
+    const password = signInForm['signin-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred);
+        alert("Hi, ", lastname)
+    })
+
+})
+
+
